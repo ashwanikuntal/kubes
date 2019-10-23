0afcf88336d9251e961af039d7367ec501e7808d
Ashwani Kuntal <ashwani.kuntal@publicisgroupe.net>
2019-10-23 13:27:19 +0530

    Kubes Commit

---
+++ src/main/java/com/example/kubes/KubesApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kubes/KubesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
